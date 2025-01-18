f43da4ec0dcf831ae568cb0fb8ba3358f1d75992
LiamJPOS <paledeepened@gmail.com>
2025-01-18 16:33:22 +0000

    Add functionality to click between images

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const selectImagesBtn = document.getElementById("select-images-btn");
 const fileInput = document.getElementById('fileInput');
 const currentImg = document.getElementById('current-img')
+const nextBtn = document.getElementById("next-btn")
+const prevBtn = document.getElementById("prev-btn")
+const reader = new FileReader();
 
+let currentImgCounter = 0;
+let files = [];
+
+//Open file dialog when button is clicked
 selectImagesBtn.addEventListener('click', () => {
     fileInput.click();
-})
+});
 
+//Handle file input change
 fileInput.addEventListener('change', () => {
-   const files = fileInput.files;
-   if (files) {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-        currentImg.src = event.target.result;
-    };
-
-    reader.readAsDataURL(files[0])
-
-   }
+    userImages = Array.from(fileInput.files);
+    if (userImages.length > 0) {
+        //Read the first image as Data URL
+        reader.readAsDataURL(userImages[currentImgCounter])
+    }
+})
+    
+nextBtn.addEventListener('click', () => {
+    if (currentImgCounter < userImages.length - 1) {
+        currentImgCounter ++;
+        reader.readAsDataURL(userImages[currentImgCounter]);
+    }
 })
 
+prevBtn.addEventListener('click', () => {
+    if (currentImgCounter > 0) {
+        currentImgCounter --;
+        reader.readAsDataURL(userImages[currentImgCounter]);
+    }
+})
+    
+//When the file is loaded, set the file as source for CurrentImg so it displays in app
+reader.addEventListener('load', () => {
+    currentImg.src = reader.result;
+})
 
+//TODO Add little graphic at the bottom that has little squares representing how many images user has loaded in and a highlight round the current one 
