765c610f1aa4f4304c18aaa5b26fd63003362d90
LiamJPOS <paledeepened@gmail.com>
2025-01-28 16:26:36 +0000

    Implement feature to display selected images and change at defined interval

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+//Settings container element
+const settingsContainer = document.getElementById("settings-container")
+//Image selection elements
+const imgSelectionContainer = document.getElementById("img-selection-container");
 const imgSelectionBtn = document.getElementById("img-selection-btn");
 const imgInput = document.getElementById("img-input");
 const imgPreviews = document.getElementById("img-previews");
 
-
-const controlsContainer = document.getElementById("controls-container");
+//Interval selection elements
+const intervalsContainer = document.getElementById("intervals-container")
 const radios = document.querySelectorAll('input[name="duration"]');
 const customIntervalBtn = document.getElementById("custom-interval-btn");
-const customInput = document.getElementById("custom-input")
+const customInput = document.getElementById("custom-input");
 
+//Start button element
+const submitContainer = document.getElementById("submit-container");
+const startBtn = document.getElementById("start-btn");
 
-let currentImgCounter = 0;
+//Main display elements
+const displayContainer = document.getElementById("display-container");
+const currentImg = document.getElementById("current-img");
+const displayControls = document.getElementById("display-controls");
+const previousBtn = document.getElementById("previous-btn");
+const pauseBtn = document.getElementById("pause-btn");
+const nextBtn = document.getElementById("next-btn");
+
 let files = []
-let userImages = [];
 let interval = 0;
 
-controlsContainer.style.display = "none";
 
 //Open file dialog when button is clicked
 imgSelectionBtn.addEventListener('click', () => {
@@ -66,3 +78,40 @@ customInput.addEventListener("input", () => {
 
     customIntervalBtn.checked = true;
   })
+
+
+
+startBtn.addEventListener("click", () => {
+  if (files.length > 0 && interval > 0) {
+    //Hide everything and display main work area and controls
+    settingsContainer.style.display = "none";
+    displayContainer.style.display = "flex";
+
+    //Load first image
+    newSrc = imgPreviews.children[0].src;
+    currentImg.src = newSrc;
+
+    //Change image on correct interval, run function for how many images there are
+    function changeImages() {
+      let currentImgCounter = 0;
+      const id = setInterval(nextImg, interval * 1000);
+      function nextImg() {
+        console.log(currentImgCounter, imgPreviews.children.length - 1);
+        if (currentImgCounter >= imgPreviews.children.length - 1) {
+          clearInterval(id);
+          currentImg.src = "Images/1000_F_122939679_d0EwmFrZpa2u6hyWvwU8TiANPvqUhTCA.jpg"
+        } else {
+          currentImgCounter ++;
+          currentImg.src = imgPreviews.children[currentImgCounter].src;
+        }
+      }
+    }
+
+    changeImages()
+
+
+  } else {
+    alert("Select files and interval length")
+    }
+  })
+
